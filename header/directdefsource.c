aea694ccffdd9cc96273fb282ba1cd8bfbbc9c8b
Jautiena <manioelvstad@gmail.com>
2025-06-10 23:51:36 +0200

    added pointer array for length + other stuff

--- header/directdefsource.c
+++ header/directdefsource.c
@@ -20,7 +20,9 @@ void clearSnake(volatile coordinates* coords) {
 	go_to(coords->X, coords->Y);
 	printf(" ");
 }
-void checkDir(char _case, volatile coordinates* multi_coords, int* STOP) {
+
+void checkDir(char _case, volatile coordinates* multi_coords, char moveDir, int* STOP) {
+	if(moveDir == 1) {
 	switch(_case) {
 		case UP:
 			if((multi_coords->direction == UP) && (STOP != NULL)) { // check for keypress in same direction as snake, and makes an exeption for NULL
@@ -33,6 +35,7 @@ void checkDir(char _case, volatile coordinates* multi_coords, int* STOP) {
 			clearSnake(multi_coords);
 			multi_coords->Y--;
 			break;
+			
 		
 		case DOWN:
 			if((multi_coords->direction == DOWN) && (STOP != NULL)) { // check for keypress in same direction as snake, and makes an exeption for NULL
@@ -68,6 +71,40 @@ void checkDir(char _case, volatile coordinates* multi_coords, int* STOP) {
 			multi_coords->direction = RIGHT;
 			clearSnake(multi_coords);
 			multi_coords->X++;
+			break;
+		
+		case ESCAPE:
+			*STOP = 1;
+	}
+	}
+	// IF MOVEDIR IS NOT TRUE, THEN JUST CHANGE DIRECTION
+	switch(_case) {
+		case UP:
+			if((multi_coords->direction == DOWN) || (multi_coords->Y == 0)) {break;} // make it so it can't turn 180 instantly, also adds logic for walls
+			if(multi_coords->direction == UP) {break;} // breaks if input is same as current direction
+			
+			multi_coords->direction = UP;
+			break;
+					
+		case DOWN:
+			if((multi_coords->direction == UP) || (multi_coords->Y == NL_MAX)) {break;} // make it so it can't turn 180 instantly, also adds logic for walls
+			if(multi_coords->direction == DOWN) {break;} // breaks if input is same as current direction
+			
+			multi_coords->direction = DOWN;
+			break;
+			
+		case LEFT:
+			if((multi_coords->direction == RIGHT) || (multi_coords->X == 0)) {break;} // make it so it can't turn 180 instantly, also adds logic for walls
+			if(multi_coords->direction == LEFT) {break;} // breaks if input is same as current direction
+			
+			multi_coords->direction = LEFT;
+			break;
+			
+		case RIGHT:
+			if((multi_coords->direction == LEFT) || (multi_coords->X == LS_MAX)) {break;} // make it so it can't turn 180 instantly, also adds logic for walls
+			if(multi_coords->direction == RIGHT) {break;} // breaks if input is same as current direction
+			
+			multi_coords->direction = RIGHT;
 			break;
 		
 		case ESCAPE:
--- snake.c
+++ snake.c
@@ -11,16 +11,33 @@
 // also figure out how to make snake longer (if too difficult, watch coding train video again)
 
 HANDLE pause;
+const int MAX_LENGTH = (NL_MAX*LS_MAX)-1;
+void hidecursor()
+{
+   HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+   CONSOLE_CURSOR_INFO info;
+   info.dwSize = 100;
+   info.bVisible = FALSE;
+   SetConsoleCursorInfo(consoleHandle, &info);
+}
 
 DWORD WINAPI move(LPVOID lpParam) {
-	volatile coordinates* multi_coords = (volatile coordinates*) lpParam;
+	volatile coordinates** multi_coords = (volatile coordinates**) lpParam;
+	WaitForSingleObject(pause, INFINITE);
+	CloseHandle(pause);
+	short Length = 0;
 	while(1) {
-		WaitForSingleObject(pause, INFINITE);
-		clearSnake(multi_coords);
-		checkDir(multi_coords->direction, multi_coords, NULL);
-		go_to(multi_coords->X, multi_coords->Y);
-		printf(SNAKE_SYM);
-		switch(multi_coords->direction) { // decide how to sleep (haahahaha funny im tired rn)
+		clearSnake(multi_coords[MAX_LENGTH-Length]);
+		for(int i = MAX_LENGTH; i > MAX_LENGTH - Length; i--) {
+		multi_coords[i-1]->X = multi_coords[i]->X;
+		multi_coords[i-1]->Y = multi_coords[i]->Y;
+		}
+		checkDir(multi_coords[MAX_LENGTH]->direction, multi_coords[MAX_LENGTH], 1, NULL);
+		for(int i = MAX_LENGTH-Length; i <= MAX_LENGTH; i++) {
+			go_to(multi_coords[i]->X, multi_coords[i]->Y);
+			printf(SNAKE_SYM);
+		}
+		switch(multi_coords[MAX_LENGTH]->direction) { // decide how to sleep (haahahaha funny im tired rn)
 			case LEFT:
 				Sleep(SLEEP_TIME);
 				break;
@@ -34,38 +51,57 @@ DWORD WINAPI move(LPVOID lpParam) {
 				Sleep(SLEEP_TIME*2);
 				break;
 		}
-		if((multi_coords->randX == multi_coords->X) && (multi_coords->randY == multi_coords->Y)) {
-		multi_coords->length++;
-		spawnFood(multi_coords);
-	}
+		if((multi_coords[MAX_LENGTH]->randX == multi_coords[MAX_LENGTH]->X) && (multi_coords[MAX_LENGTH]->randY == multi_coords[MAX_LENGTH]->Y)) { // check if coords match, eat food and spawn new
+			Length = multi_coords[MAX_LENGTH]->length++;
+			spawnFood(multi_coords[MAX_LENGTH]);
+		}
 	}
 	return 0;
 }
 
 int main() {
-volatile coordinates* multi_coords = (coordinates*) malloc(sizeof(coordinates));
-multi_coords->X = LS_MAX/2;
-multi_coords->Y = 2;
-multi_coords->length = 1;
-multi_coords->direction = -1;
+volatile coordinates** multi_coords = (volatile coordinates**) malloc(sizeof(coordinates*)*MAX_LENGTH+1); // allocating double pointer
+for(int i = 0; i <= MAX_LENGTH; i++) { // allocating each struct
+	multi_coords[i] = malloc(sizeof(coordinates));
+}
+if(multi_coords == NULL) {
+	printf("ERROR ALLOCATING MEMORY\n");
+	return -1;
+}
+multi_coords[MAX_LENGTH]->X = LS_MAX/2;
+multi_coords[MAX_LENGTH]->Y = 2;
+multi_coords[MAX_LENGTH]->length = 0;
+multi_coords[MAX_LENGTH]->direction = -1;
 int stop = 0;
 pause = CreateEvent(NULL, TRUE, FALSE, NULL);
 
-system("cls");
-go_to(multi_coords->X, multi_coords->Y); // set cursor to coordinates
+hidecursor();
 
-spawnFood(multi_coords); // spawn food at random coordinates (TODO: GET BETTER RANDOM COORDINATES)
-HANDLE thread1 = CreateThread(NULL, 0, move, (coordinates*) multi_coords, 0, NULL);
+system("cls");
+go_to(multi_coords[MAX_LENGTH]->X, multi_coords[MAX_LENGTH]->Y); // set cursor to coordinates
+printf(SNAKE_SYM);
+
+spawnFood(multi_coords[MAX_LENGTH]); // spawn food at random coordinates (TODO: GET BETTER RANDOM COORDINATES)
+HANDLE thread_2 = CreateThread(NULL, 0, move, (LPVOID) multi_coords, 0, NULL); // start new thread
+if(thread_2 == NULL) {
+	printf("ERROR: Could not create thread\n");
+	return -1;
+}
+checkDir(getch(), multi_coords[MAX_LENGTH], 0, &stop); // check first move
+SetEvent(pause); // signal pause over, so second thread can get moving
 
 while(1) {
-	go_to(multi_coords->X, multi_coords->Y);
+	go_to(multi_coords[MAX_LENGTH]->X, multi_coords[MAX_LENGTH]->Y);
 	printf(SNAKE_SYM);
-	checkDir(getch(), multi_coords, &stop);
-	SetEvent(pause);
+	checkDir(getch(), multi_coords[MAX_LENGTH], 0, &stop);
 	if(stop) {
 		system("cls");
-		free((coordinates *) multi_coords); // have to do (coordinates *) to avoid volatile warning
-		CloseHandle(thread1);
+		printf("FINAL LENGTH: %d\n", multi_coords[MAX_LENGTH]->length);
+		for(int i = 0; i <= MAX_LENGTH; i++) { // freeing each struct
+			free((coordinates*) multi_coords[i]); // have to do (coordinates *) to avoid compiler warning
+		}
+		free((coordinates **) multi_coords); // have to do (coordinates **) to avoid compiler warning
+		CloseHandle(thread_2);
 		return 0;
 	}
 	
